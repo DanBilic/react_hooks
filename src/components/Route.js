17ad8665e11a99145e308feb89516f7780455123
DanBilic <bilic90@gmx.de>
2020-08-08 15:00:13 +0200

    implemented own Link component for navigation

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,24 @@
 //implement own routing
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 //cildren prop -> the children components of Route component
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      //just to rerender the Route component when the url changes
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+
+  return currentPath === path ? children : null;
 };
 
 export default Route;
